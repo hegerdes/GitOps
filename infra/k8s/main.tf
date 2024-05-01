14b77fe49eaf571b8c508c0aabb99216f3d17aac
Henrik Gerdes <hegerdes@outlook.de>
2024-05-01 23:41:56 +0200

    fix: tf namespace name constraint
    Signed-off-by: Henrik Gerdes <hegerdes@outlook.de>

--- infra/k8s/main.tf
+++ infra/k8s/main.tf
@@ -1,10 +1,12 @@
 resource "kubernetes_namespace" "example" {
   metadata {
-    name = random_string.random.result
+    name = lower(random_string.random.result)
   }
 }
 
 resource "random_string" "random" {
   length  = 8
+  upper   = false
   special = false
+  numeric = false
 }
