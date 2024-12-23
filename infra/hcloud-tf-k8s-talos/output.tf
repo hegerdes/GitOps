1358c27a76de31146b73abdce014ac5bb7641b3a
Henrik Gerdes <hegerdes@outlook.de>
2024-12-23 22:05:31 +0100

    chore: remove unused kubeconf
    Signed-off-by: Henrik Gerdes <hegerdes@outlook.de>

--- infra/hcloud-tf-k8s-talos/output.tf
+++ infra/hcloud-tf-k8s-talos/output.tf
@@ -8,35 +8,10 @@ resource "local_sensitive_file" "talosclientconf" {
   content  = data.talos_client_configuration.this.talos_config
   filename = "out/talosconfig.yaml"
 }
+
 resource "local_sensitive_file" "kubeconf" {
+  content  = talos_cluster_kubeconfig.this.kubeconfig_raw
   filename = "out/kubeconfig.yaml"
-  content = yamlencode({
-    apiVersion = "v1"
-    kind       = "Config"
-    clusters = [{
-      name = local.cluster_name
-      cluster = {
-        server                     = "https://${local.cp_public_endpoint}:6443"
-        certificate-authority-data = talos_cluster_kubeconfig.this.kubernetes_client_configuration.ca_certificate
-      }
-    }]
-    users = [{
-      name = "admin@${local.cluster_name}"
-      user = {
-        client-certificate-data = talos_cluster_kubeconfig.this.kubernetes_client_configuration.client_certificate
-        client-key-data         = talos_cluster_kubeconfig.this.kubernetes_client_configuration.client_key
-      }
-    }]
-    contexts = [{
-      name = "admin@${local.cluster_name}"
-      context = {
-        cluster   = local.cluster_name
-        namespace = "default"
-        user      = "admin@${local.cluster_name}"
-      }
-    }]
-    current-context = "admin@${local.cluster_name}"
-  })
 }
 
 output "certSANs" {
