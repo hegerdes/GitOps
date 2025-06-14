8c296c5098483b058a58a1359be0a12e4c96efc3
Henrik Gerdes <hegerdes@outlook.de>
2025-06-14 18:25:30 +0200

    deps: bump to talos v1.10.4
    Signed-off-by: Henrik Gerdes <hegerdes@outlook.de>

--- infra/hcloud-tf-k8s-talos/variables.tf
+++ infra/hcloud-tf-k8s-talos/variables.tf
@@ -10,7 +10,7 @@ variable "cluster_version" {
 }
 variable "talos_version" {
   type        = string
-  default     = "v1.10.3"
+  default     = "v1.10.4"
   description = "Talos Version of the cluster."
 }
 variable "controlplane_endpoint" {
