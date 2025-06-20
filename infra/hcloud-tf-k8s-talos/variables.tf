c8b41df6f3baec9e16657100a3ff2daa801bf5b4
Henrik Gerdes <hegerdes@outlook.de>
2025-06-20 22:25:45 +0200

    deps: bump to k8s v1.33.2
    Signed-off-by: Henrik Gerdes <hegerdes@outlook.de>

--- infra/hcloud-tf-k8s-talos/variables.tf
+++ infra/hcloud-tf-k8s-talos/variables.tf
@@ -5,7 +5,7 @@ variable "cluster_name" {
 }
 variable "cluster_version" {
   type        = string
-  default     = "v1.33.1"
+  default     = "v1.33.2"
   description = "Version of the cluster."
 }
 variable "talos_version" {
