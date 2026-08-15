46aa04192d9c32586342b10283646643433f0b3a
Henrik Gerdes <hegerdes@outlook.de>
2026-08-15 12:59:17 +0200

    deps: bum tf & packer hcloud provider
    Signed-off-by: Henrik Gerdes <hegerdes@outlook.de>

--- infra/hcloud-packer-kubeadm/debian.pkr.hcl
+++ infra/hcloud-packer-kubeadm/debian.pkr.hcl
@@ -4,7 +4,7 @@ packer {
   required_plugins {
     hcloud = {
       source  = "github.com/hetznercloud/hcloud"
-      version = ">= 1.7.0"
+      version = "~>1.8"
     }
   }
 }
--- infra/hcloud-tf-k8s-kubeadm/provider.tf
+++ infra/hcloud-tf-k8s-kubeadm/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "~>1.66"
+      version = "~>1.68"
     }
     random = {
       source  = "hashicorp/random"
